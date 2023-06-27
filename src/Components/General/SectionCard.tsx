4841ccb3bef798d5a1e790f059d59925bfba978e
KiritoKing <kiritoclzh@gmail.com>
2023-06-28 00:35:39 +0800

    wip: 添加背景动效果

--- src/Components/General/SectionCard.tsx
+++ src/Components/General/SectionCard.tsx
@@ -3,7 +3,15 @@ import React, { PropsWithChildren } from "react";
 
 const SectionCard: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Card variant="outlined" sx={{ width: "100%", p: 5 }}>
+    <Card
+      variant="outlined"
+      sx={{
+        width: "100%",
+        p: 5,
+        backdropFilter: "blur(10px)",
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+      }}
+    >
       {children}
     </Card>
   );
