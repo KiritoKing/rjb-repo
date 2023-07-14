670efabf313494b9bb7f91e5833aaf4512470a8b
KiritoKing <kiritoclzh@gmail.com>
2023-07-15 00:08:15 +0800

    chore: 修改toast持续时间

--- src/main.tsx
+++ src/main.tsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CssVarsProvider>
-      <Toaster closeButton richColors position="top-left" />
+      <Toaster duration={1000} closeButton richColors position="top-left" />
       <RouterProvider router={router} />
     </CssVarsProvider>
   </React.StrictMode>
